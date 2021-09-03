8f16b756cce1b131f097abf7fa03ce25c4769373
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2021-09-03 22:41:31 +1200

    Add tests for options param

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ const sinon = require('sinon')
 const flydDomEvents = require('./index')
 const flyd = require('flyd')
 
-tape('Calling with correct params', test => {
+tape('Calling with good params', test => {
   test.plan(7)
 
-  const types = ['mouseclick', 'contextmenu']
+  const types = ['click', 'contextmenu']
   const target = {
     addEventListener: sinon.spy(),
     removeEventListener: sinon.spy()
@@ -18,7 +18,7 @@ tape('Calling with correct params', test => {
   test.ok(flyd.isStream(stream), 'returns a flyd stream')
   test.ok(
     target.addEventListener.calledTwice &&
-      target.addEventListener.calledWith('mouseclick', stream, options) &&
+      target.addEventListener.calledWith('click', stream, options) &&
       target.addEventListener.calledWith('contextmenu', stream, options),
     'attaches listeners'
   )
@@ -31,7 +31,7 @@ tape('Calling with correct params', test => {
   const listener = sinon.spy()
   const {
     firstCall: {
-      args: [, onmouseclick] // function that would be triggered by an onmouseclick DOM event
+      args: [, onclick] // function that would be triggered by an onclick DOM event
     },
     secondCall: {
       args: [, oncontextmenu]
@@ -41,8 +41,8 @@ tape('Calling with correct params', test => {
   stream.map(listener)
   test.ok(listener.notCalled, 'does not trigger early')
 
-  const fakeEvent1 = { type: 'mouseclick' }
-  onmouseclick(fakeEvent1)
+  const fakeEvent1 = { type: 'click' }
+  onclick(fakeEvent1)
   test.ok(
     listener.calledOnceWith(fakeEvent1),
     'triggers events correctly (1/2)'
@@ -59,8 +59,30 @@ tape('Calling with correct params', test => {
   stream.end(true)
   test.ok(
     target.removeEventListener.calledTwice &&
-      target.removeEventListener.calledWith('mouseclick', stream, options) &&
+      target.removeEventListener.calledWith('click', stream, options) &&
       target.removeEventListener.calledWith('contextmenu', stream, options),
+    'detaches listeners'
+  )
+})
+
+tape('Calling without optional options param', test => {
+  test.plan(2)
+
+  const types = ['pointerdown']
+  const target = {
+    addEventListener: sinon.spy(),
+    removeEventListener: sinon.spy()
+  }
+
+  const stream = flydDomEvents(types, target)
+  stream.end(true)
+
+  test.ok(
+    target.addEventListener.calledOnceWith('pointerdown', stream),
+    'attaches listeners'
+  )
+  test.ok(
+    target.removeEventListener.calledOnceWith('pointerdown', stream),
     'detaches listeners'
   )
 })
